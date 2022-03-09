f05d572dcdd4e303d228b1d0e836b60a68fc75c1
Willerjls <willer.jacques555@gmail.com>
2022-03-09 13:09:59 -0300

    formulário de episódios

--- js/adicionar.js
+++ js/adicionar.js
@@ -1,10 +1,16 @@
-function readImage(){
-  if(this.file && this.file[0]){
-    var file = new FileReader();
-    file.onload = function(e){
-      document.getElementById("preview").src = e.target.result;
-    };
-    file.readAsDataURL(this.files[0]);
+function previewImagem(){
+  var imagem = document.querySelector('input[name=imagem]').files[0];
+  var preview = document.querySelector('.img');
+
+  var reader = new FileReader();
+
+  reader.onloadend = function(){
+    preview.src = reader.result;
+  }
+
+  if(imagem){
+    reader.readAsDataURL(imagem);
+  }else{
+    preview.src = ""
   }
 }
-document.getElementById("img-input").addEventListener("change", readImage, false)
